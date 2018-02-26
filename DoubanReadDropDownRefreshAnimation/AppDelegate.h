0e8926b896995282b769afa28511c6dec5e0cba7
wangwenbo <wangwenbo1@kingsoft.com>
2018-02-26 17:54:27 +0800

    Initial Commit

---
+++ DoubanReadDropDownRefreshAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubanReadDropDownRefreshAnimation
+//
+//  Created by wangwenbo on 2018/2/26.
+//  Copyright © 2018年 wangwenbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
